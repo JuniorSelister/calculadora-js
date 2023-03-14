93d7db0dfd3740692f1497ca99884611a85dc6fd
JuniorSelister <junior.selister@gmail.com>
2023-03-14 14:52:16 -0300

    Uploading project

---
+++ main.js
@@ -0,0 +1,12 @@
+import './styles.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div className="calculator">
+      <div class="history"></div>
+      <div className="results"></div>
+      
+    </div>
+
+  </div>
+`
